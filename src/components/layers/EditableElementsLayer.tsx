c092e8a31ebdf49ff9a81e704a7fdacb56a3d710
Xavi-Arnau <chavier28@gmail.com>
2025-01-17 11:32:53 +0100

    wip tools

--- src/components/layers/EditableElementsLayer.tsx
+++ src/components/layers/EditableElementsLayer.tsx
@@ -2,61 +2,94 @@
 //testing the leaflet-editable inside of react-leaflet structure
 //https://leaflet.github.io/Leaflet.Editable/doc/api.html
 //https://codesandbox.io/p/sandbox/react-leaflet-ctrl-click-7cs0u?file=%2Fsrc%2Findex.js
-import { useMap } from "react-leaflet";
+import { FeatureGroup, useMap } from "react-leaflet";
 import "leaflet-editable";
-import { useState } from "react";
-import { Square, Minus, Circle, RectangleHorizontal } from "lucide-react";
+import { useRef } from "react";
+import {
+  Square,
+  Minus,
+  Circle,
+  RectangleHorizontal,
+  Save,
+  Eraser,
+} from "lucide-react";
 
 const EditableElementsLayer = () => {
-  const [shapes, setShapes] = useState([]);
   const parentMap = useMap();
 
+  const featureGroupRef = useRef();
+
   const newPolyLine = () => {
-    parentMap.editTools.startPolyline(null);
+    const newShape = parentMap.editTools.startPolyline(null);
+    featureGroupRef.current.addLayer(newShape);
   };
   const newCircle = () => {
-    parentMap.editTools.startCircle(null);
+    const newShape = parentMap.editTools.startCircle(null);
+    featureGroupRef.current.addLayer(newShape);
   };
 
   const startRectangle = () => {
-    parentMap.editTools.startRectangle(null);
+    const newShape = parentMap.editTools.startRectangle(null);
+    featureGroupRef.current.addLayer(newShape);
   };
 
   const newPolygon = () => {
-    const polygon = parentMap.editTools.startPolygon(null);
-    setShapes([...shapes, polygon]);
+    const newShape = parentMap.editTools.startPolygon(null);
+    featureGroupRef.current.addLayer(newShape);
   };
-  console.log(shapes);
+
+  const deleteAll = () => {
+    featureGroupRef.current.clearLayers();
+  };
+
+  const saveAll = () => {
+    const geoJSON = featureGroupRef.current.toGeoJSON();
+    console.log(geoJSON);
+  };
 
   return (
-    <div className="leaflet-top leaflet-right">
-      <div className="leaflet-control flex flex-col gap-2">
-        <button
-          onClick={newPolyLine}
-          className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
-        >
-          <Minus />
-        </button>
-        <button
-          onClick={newPolygon}
-          className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
-        >
-          <Square />
-        </button>
-        <button
-          onClick={newCircle}
-          className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
-        >
-          <Circle />
-        </button>
-        <button
-          onClick={startRectangle}
-          className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
-        >
-          <RectangleHorizontal />
-        </button>
+    <FeatureGroup ref={featureGroupRef}>
+      <div className="leaflet-top leaflet-right">
+        <div className="leaflet-control flex flex-col gap-2 bg-slate-100 p-2 rounded-xl">
+          <button
+            onClick={newPolyLine}
+            className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
+          >
+            <Minus />
+          </button>
+          <button
+            onClick={newPolygon}
+            className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
+          >
+            <Square />
+          </button>
+          <button
+            onClick={newCircle}
+            className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
+          >
+            <Circle />
+          </button>
+          <button
+            onClick={startRectangle}
+            className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
+          >
+            <RectangleHorizontal />
+          </button>
+          <button
+            onClick={deleteAll}
+            className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
+          >
+            <Eraser />
+          </button>
+          <button
+            onClick={saveAll}
+            className="bg-black text-white p-2 text-3xl hover:scale-110 rounded-xl"
+          >
+            <Save />
+          </button>
+        </div>
       </div>
-    </div>
+    </FeatureGroup>
   );
 };
 
