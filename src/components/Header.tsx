91ad821b537a9b4eeaeae7d88682b867cc419793
Xavi-Arnau <chavier28@gmail.com>
2025-01-20 22:18:19 +0100

    responsive menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,54 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 
 const Header = () => {
   return (
     <div className="bg-black text-white w-full">
-      <div className="flex flex-col md:flex-row w-10/12 mx-auto gap-4 items-center justify-between p-4">
+      <div className="flex flex-row md:flex-row w-10/12 mx-auto gap-4 items-center justify-between p-4">
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <Menu />
+            </SheetTrigger>
+            <SheetContent
+              side={"left"}
+              className="text-white bg-black z-[9999]"
+            >
+              <SheetHeader>
+                <SheetDescription>
+                  <ul className="flex flex-col gap-2 w-40 p-4 text-left">
+                    <li>
+                      <Link to="/">Maps</Link>
+                    </li>
+                    <li>
+                      <Link to="/testwms">WMS Map</Link>
+                    </li>
+
+                    <li>
+                      <Link to="/editable">Leaflet-Editable</Link>
+                    </li>
+                    <li>
+                      <Link to="/editablegeoman">Geoman</Link>
+                    </li>
+                  </ul>
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
         <div className="text-2xl">NRAMS SandBox</div>
 
-        <ul className="flex flex-col md:flex-row gap-4">
+        <ul className="hidden md:flex flex-col md:flex-row gap-4 ">
           <NavigationMenu>
             <NavigationMenuList>
               <NavigationMenuItem>
