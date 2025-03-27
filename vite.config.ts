cb7dcee2794a3fbd721810325b0615f6a075efc6
Xavi-Arnau <chavier28@gmail.com>
2025-03-27 17:11:52 +0100

    dockerized with pro dev and staging builds

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,24 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-    watch: {
-      usePolling: true,
+export default defineConfig(() => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the
+  // `VITE_` prefix.
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: true,
+      watch: {
+        usePolling: true,
+      },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
